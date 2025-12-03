f1ea92c1250599f6fe87a721399f4784277a1dc6
t1ar <80429306+t1ar@users.noreply.github.com>
2025-12-03 21:00:19 +0700

    Add global variables and main structure
    Introduces global variables, struct definitions, and function declarations for the vending machine program. Sets up the main function to initialize data from files and call the main menu. Adds placeholders for function implementations.

--- VendingMachine.cpp
+++ VendingMachine.cpp
@@ -11,3 +11,43 @@
 #include <fstream> //file handling for write/read
 #include <string> // TODO : i forgot what string for, pls write 
 using namespace std;
+
+//global variables
+const int MAX = 9;
+int Wallet = 20000;    //buyer's wallet
+int Money, pilihan;   // Mr. Krabs : Money! | global var 
+string Nama_Item, Jumlah, Harga;
+struct structItem { 
+    string Nama;
+    int Jumlah;
+    int Harga;
+} Item[MAX], Storage[MAX];
+
+//Function declarations
+void BuyStock();
+void Restock();
+void Beli();
+void StockView();
+void Pilih();
+void Menu();
+void read();
+
+//main program
+int main() {
+    ifstream profit ("Profit.txt"), 
+    stock ("Stock.txt"), 
+    warehouse ("Storage.txt"); // initialize data from file, Money for profit, structItem for stock,
+    
+    profit >> Money; //get the line for MONEYYYYYY
+    profit.close();
+    read(Item, stock);
+    stock.close();
+    read(Storage, warehouse);
+    warehouse.close();
+    
+    Menu();
+    cout << "\nGoodbye!\n"; 
+}
+
+//TODO : complete all function definitions below
+//Should we reorder the functions?
